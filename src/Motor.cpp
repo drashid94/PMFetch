fadae940971965c1e4a23c420ed18a706b1dbfa2
Danyal Rashid <drashid@sfu.ca>
2024-07-12 03:02:11 -0700

    Accel Decel Uncompiled

--- src/Motor.cpp
+++ src/Motor.cpp
@@ -62,19 +62,109 @@ struct MovementInfo
 
 Motor::Motor() { }
 
+void motorRampUp(MovementInfo *movementInfo)
+{
+    uint32_t motorPin = movementInfo->motorPin; 
+    uint32_t motorDirPin = movementInfo->motorDirPin;
+    uint32_t direction = movementInfo->direction;
+    uint32_t pulses = movementInfo->pulses;
+    uint32_t motorSpeedFinal = movementInfo->motorSpeed;
+    bool pollSensor = movementInfo->pollSensor;
+
+    uint32_t segments = 10;
+    uint32_t numPulsesPerSeg = pulses/segments;
+    uint32_t motorSpeed = 10*motorSpeed;
+
+    for(uint32_t i = 0; i < segments; i++)
+    {
+        for(uint32_t i = 0; i < numPulsesPerSeg; i++)
+        {
+            if(pollSensor && pollCounter > 4)
+            {
+                bool contactVal = 0;
+                if(motorPin == X_MOTOR_PIN)
+                    get_x_sensor_value(contactVal);
+                if(motorPin == Y_MOTOR_PIN)
+                    get_y_sensor_value(contactVal);
+                if(motorPin == Z_MOTOR_PIN)
+                    get_z_sensor_value(contactVal);
+                if(contactVal)
+                {
+                    std::cout << "Contact Sensor Pushed\nExiting movement function\n";
+                    break;
+                }
+                pollCounter = 0;
+            }
+            lgGpioWrite(h, motorPin, 1);
+            usleep(motorSpeed);
+            lgGpioWrite(h, motorPin, 0);
+            usleep(motorSpeed);
+            pollCounter++;
+        }
+        motorSpeed -= motorSpeedFinal;
+    }
+    std::cout << "Final Motor Speed Reached during ramp up: " << motorSpeed << "\n";
+}
+
+void motorRampDown(MovementInfo *movementInfo)
+{
+    uint32_t motorPin = movementInfo->motorPin; 
+    uint32_t motorDirPin = movementInfo->motorDirPin;
+    uint32_t direction = movementInfo->direction;
+    uint32_t pulses = movementInfo->pulses;
+    uint32_t motorSpeed = movementInfo->motorSpeed;
+    bool pollSensor = movementInfo->pollSensor;
+
+    uint32_t segments = 10;
+    uint32_t numPulsesPerSeg = pulses/segments;
+    for(uint32_t i = 0; i < segments; i++)
+    {
+        for(uint32_t i = 0; i < numPulsesPerSeg; i++)
+        {
+            if(pollSensor && pollCounter > 4)
+            {
+                bool contactVal = 0;
+                if(motorPin == X_MOTOR_PIN)
+                    get_x_sensor_value(contactVal);
+                if(motorPin == Y_MOTOR_PIN)
+                    get_y_sensor_value(contactVal);
+                if(motorPin == Z_MOTOR_PIN)
+                    get_z_sensor_value(contactVal);
+                if(contactVal)
+                {
+                    std::cout << "Contact Sensor Pushed\nExiting movement function\n";
+                    break;
+                }
+                pollCounter = 0;
+            }
+            lgGpioWrite(h, motorPin, 1);
+            usleep(motorSpeed);
+            lgGpioWrite(h, motorPin, 0);
+            usleep(motorSpeed);
+            pollCounter++;
+        }
+        motorSpeed += motorSpeed;
+    }
+    std::cout << "Final Motor Speed Reached during ramp down: " << motorSpeed << "\n";
+}
+
 void *moveFunc(void * moveParams)
 {
     MovementInfo *movementInfo;
     movementInfo = (MovementInfo*)moveParams;
-    uint32_t motorPin = movementInfo.motorPin; 
-    uint32_t motorDirPin = movementInfo.motorDirPin;
-    uint32_t direction = movementInfo.direction;
-    uint32_t pulses = movementInfo.pulses;
-    uint32_t motorSpeed = movementInfo.motorSpeed;
-    bool pollSensor = movementInfo.pollSensor;
+    uint32_t motorPin = movementInfo->motorPin; 
+    uint32_t motorDirPin = movementInfo->motorDirPin;
+    uint32_t direction = movementInfo->direction;
+    uint32_t pulsesTotal = movementInfo->pulses;
+    uint32_t motorSpeed = movementInfo->motorSpeed;
+    bool pollSensor = movementInfo->pollSensor;
     uint32_t pollCounter = 0;
-    cout << "Creating " << pulses << " pulses\n";
-    for(uint32_t i = 0; i < pulses; i++)
+    std::cout << "Creating " << pulses << " total pulses\n";
+
+    uint32_t rampPulses = .2 * pulsesTotal;
+    movementInfo->pulses = rampPulses/2;
+    motorRampUp(movementInfo);
+    for(uint32_t i = 0; i < pulsesTotal - rampPulses; i++)
     {
         if(pollSensor && pollCounter > 4)
         {
@@ -97,7 +187,8 @@ void *moveFunc(void * moveParams)
         lgGpioWrite(h, motorPin, 0);
         usleep(motorSpeed);
         pollCounter++;
-    }
+    }    
+    motorRampDown(movementInfo);
     pthread_exit(NULL);
 }
 
